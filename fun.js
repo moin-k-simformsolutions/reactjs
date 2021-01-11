899a1b01d5b734985362e038ad1e821ddde02cbf
moinmk <moin.k@simformsolutions.com>
2021-01-11 18:15:26 +0530

    adding files

---
+++ fun.js
@@ -0,0 +1,13 @@
+import React from "react"
+function Fun(){
+    var name="mk"
+    return (<div>
+    <h1 className="mk" style={{color:"red"}}>name is: {name}</h1>
+    <p>
+        hi! i'm mk. i am learning react.
+    </p>
+
+    </div>);
+}
+
+export default Fun
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fun from "./fun.js"
+ReactDOM.render(<h1>hello world!</h1>, document.getElementById('root'));
+ReactDOM.render(<Fun />, document.getElementById('root'));
