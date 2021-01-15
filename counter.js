f7196514858cb85efb1f14a29a637d3233580672
moinmk <moin.k@simformsolutions.com>
2021-01-15 16:05:18 +0530

    hooks with stateless function

--- counter.js
+++ counter.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.increment = this.increment.bind(this);
-        this.state = { count: 0 }
-    }
-    render() {
-        return (
-            <div style={{ textAlign: "center", marginTop: "20%" }}>
-                <p style={{ fontSize: "30px" }}>total count:{this.state.count}</p>
-                <button onClick={this.increment} style={{fontSize:"15px"}}>increment</button>
-            </div>
-        );
-    }
+// class Counter extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         // this.increment = this.increment.bind(this);
+//         this.state = { count: 0 }
+//     }
+//     render() {
+//         return (
+//             <div style={{ textAlign: "center", marginTop: "20%" }}>
+//                 <p style={{ fontSize: "30px",color:"white" }}>total count:{this.state.count}</p>
+//                 <button onClick={()=>this.setState({ count: this.state.count + 1 })} style={{fontSize:"15px"}}>increment</button>
+//             </div>
+//         );
+//     }
 
-    increment() {
-        this.setState({ count: this.state.count + 1 });
-    }
+//     // increment=()=> {
+//     //     this.setState({ count: this.state.count + 1 });
+//     // }
+// }
+
+
+
+//===== use of hooks with stateless functions=====/
+//because of hooks we can state in functional components 
+const Counter = () => {
+    const [count, incrementCount] = useState(0);
+    return (
+        <div style={{ textAlign: "center", marginTop: "20%" }}>
+            <p style={{ fontSize: "30px", color: "white" }}>total count:{count}</p>
+            <button onClick={() => incrementCount(count+1)} style={{ fontSize: "15px" }}>increment</button>
+        </div>
+    );
 }
 
 export default Counter;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Frineds from "./friends.js"
-// import Counter from "./counter"
-import AllShows from "./shows/allShows"
+import Counter from "./counter"
+// import AllShows from "./shows/allShows"
 // ReactDOM.render(<h1>hello world!</h1>, document.getElementById('root'));
 // ReactDOM.render(<Fun name="mk"/>, document.getElementById('root'));
 // ReactDOM.render(<Frineds/>, document.getElementById('root'));
-// ReactDOM.render(<Counter/>, document.getElementById('root'));
-ReactDOM.render(<AllShows />, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
+// ReactDOM.render(<AllShows />, document.getElementById('root'));
